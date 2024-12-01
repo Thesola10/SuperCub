296bf825e465211a52dae621f4ef2a993f41436b
TheSola10 <me@thesola.io>
2024-12-01 15:37:45 +0100

    add stdin support

--- src/bin/cub-pp.rs
+++ src/bin/cub-pp.rs
@@ -2,6 +2,7 @@
 //! Takes a single file as input (or stdin), outputs resulting C code to stdout.
 
 use std::fs;
+use std::io;
 use std::env;
 use std::error::Error;
 
@@ -10,12 +11,17 @@ use supercub::realize;
 fn main() -> Result<(), Box<dyn Error>>{
     let args: Vec<String> = env::args().collect();
 
-    let fname = &args[1];
 
     //TODO: hopefully we won't need this in the future
     env_logger::init();
 
-    let src: String = fs::read_to_string(fname)?;
+    let src: String;
+    if args.len() >= 2 {
+        let fname = &args[1];
+        src = fs::read_to_string(fname)?;
+    } else {
+        src = io::read_to_string(io::stdin())?;
+    }
     println!("{}", realize(&src, Vec::new()));
     Ok(())
 }
